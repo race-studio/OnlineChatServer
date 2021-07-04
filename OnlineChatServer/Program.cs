a38d7e8c5cfe41d761647818800bdeb0bc5d467a
BuildTools <unconfigured@null.spigotmc.org>
2021-07-04 22:37:51 +0300

    Добавьте файлы проекта.

---
+++ OnlineChatServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnlineChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
